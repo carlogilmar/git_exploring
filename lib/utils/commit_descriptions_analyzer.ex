313d0acfc50c0d8a23667b687d6b8bed08cf68bc
carlogilmar <carlogilmar12@gmail.com>
2019-02-14 23:49:17 -0600

    Refactor analyzer

--- lib/utils/commit_descriptions_analyzer.ex
+++ lib/utils/commit_descriptions_analyzer.ex
@@ -1,13 +1,19 @@
-defmodule GitExploring.CommitDescriptionsAnalizer do
+defmodule GitExploring.CommitDescriptionsAnalyzer do
 
-  def get_words_from_description( { _hash, _author, _date, description} ) do
-    words = description |> String.split(" ")
-    for w <- words, do: String.downcase( w )
+  def analyzer_commits_words( commits ) do
+    commits
+      |> get_words_from_commits()
+      |> get_words_counters()
   end
 
   def get_words_from_commits( commits ) do
     words_list = for c <- commits, do: get_words_from_description( c )
     words_list |> List.flatten()
+  end
+
+  def get_words_from_description( { _hash, _author, _date, description} ) do
+    words = description |> String.split(" ")
+    for w <- words, do: String.downcase( w )
   end
 
   def get_words_counters( words ) do
--- test/commit_descriptions_analyzer_test.ex
+++ test/commit_descriptions_analyzer_test.ex
@@ -1,23 +1,23 @@
-defmodule CommitDescriptionsAnalizerTest do
+defmodule CommitDescriptionsAnalyzerTest do
   use ExUnit.Case
-  alias GitExploring.CommitDescriptionsAnalizer
+  alias GitExploring.CommitDescriptionsAnalyzer
 
   test "Analyze a commit description" do
     commit = {"aed0dbf7963a37aab277e083c22b616863f04773", "carlogilmar12@gmail.com", "Thu Jan 31 10:20:09 2019 -0600", "#60 Removing get_initials from patient and responsible"}
-    words = CommitDescriptionsAnalizer.get_words_from_description( commit )
+    words = CommitDescriptionsAnalyzer.get_words_from_description( commit )
     assert words == ["#60", "removing", "get_initials", "from", "patient", "and", "responsible"]
   end
 
   test "Get all the words by a list of commits" do
     c = {"", "", "", "#60 Removing get_initials from patient and responsible"}
     commits = [c,c,c,c]
-    words_list = CommitDescriptionsAnalizer.get_words_from_commits( commits )
+    words_list = CommitDescriptionsAnalyzer.get_words_from_commits( commits )
     assert length(words_list) == 28
   end
 
   test "Count the times that a word appear" do
     words = ["word1", "word2", "word3", "word2", "word3", "word1", "word2"]
-    words_counters = CommitDescriptionsAnalizer.get_words_counters( words )
+    words_counters = CommitDescriptionsAnalyzer.get_words_counters( words )
     words_countes_expected = [ {"word1", 2},  {"word3", 2},  {"word2", 3} ]
     assert words_counters == words_countes_expected
   end
