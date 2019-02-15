04c60fe457c34a15af8640cf5e9ab3a093cbeff7
carlogilmar <carlogilmar12@gmail.com>
2019-02-14 23:18:52 -0600

    Test get words from description function

--- lib/utils/commit_descriptions_analizer.ex
+++ lib/utils/commit_descriptions_analizer.ex
@@ -1,2 +1,8 @@
-defmodule CommitDescriptionsAnalizer do
+defmodule GitExploring.CommitDescriptionsAnalizer do
+
+  def get_words_from_description( { _hash, _author, _date, description} ) do
+    words = description |> String.split(" ")
+    for w <- words, do: String.downcase( w )
+  end
+
 end
--- test/commit_descriptions_analyzer_test.ex
+++ test/commit_descriptions_analyzer_test.ex
@@ -2,7 +2,10 @@ defmodule CommitDescriptionsAnalizerTest do
   use ExUnit.Case
   alias GitExploring.CommitDescriptionsAnalizer
 
-  test "Format a log output" do
-    assert 1 == 2
+  test "Analyze a commit description" do
+    commit = {"aed0dbf7963a37aab277e083c22b616863f04773", "carlogilmar12@gmail.com", "Thu Jan 31 10:20:09 2019 -0600", "#60 Removing get_initials from patient and responsible"}
+    words = CommitDescriptionsAnalizer.get_words_from_description( commit )
+    assert words == ["#60", "removing", "get_initials", "from", "patient", "and", "responsible"]
   end
+
 end
