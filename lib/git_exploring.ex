8944555967a59d56ac1be26d66af28feb4f59172
Carlo Gilmar <carlogilmar12@gmail.com>
2019-05-14 00:31:44 -0500

    Refactor main module

--- lib/git_exploring.ex
+++ lib/git_exploring.ex
@@ -3,17 +3,15 @@ defmodule GitExploring do
   alias GitExploring.Menu
 
   def start_analysis(file_path, command) do
-    set_menu = fn
-      {:commits} -> Menu.reading_git_log(file_path)
-      {:words} -> Menu.getting_most_useful_words(file_path)
-      {:branches} -> Menu.getting_all_branches(file_path)
-      {:commits_by_branch, reference} -> Menu.describe_branch(file_path, reference)
+    apply_menu = fn
+      {"", _command} -> raise "Git Project not found! Please select a project using git..."
+      {_git, {:commits}} -> Menu.reading_git_log(file_path)
+      {_git, {:words}} -> Menu.getting_most_useful_words(file_path)
+      {_git, {:branches}} -> Menu.getting_all_branches(file_path)
+      {_git, {:commits_by_branch, reference}} -> Menu.describe_branch(file_path, reference)
     end
     resp = Menu.apply_git_command(["status"], file_path)
-    case resp do
-      "" -> raise "Git Project not found! Please select a project using git..."
-      _git -> set_menu.(command)
-    end
+    apply_menu.({resp, command})
   end
 
 end
